4f4d7f70b4f7c7e523d8c5c005a96ec48a897fb7
André Muniz <andre250899@gmail.com>
2021-10-14 23:44:52 -0300

    added remove like function

--- src/App.tsx
+++ src/App.tsx
@@ -23,5 +23,3 @@ function App() {
 }
 
 export default App;
-
-//57m
--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -28,7 +28,7 @@ type QuestionType = {
   isAnswered: boolean;
   isHighlighted: boolean;
   likeCount: number;
-  hasLiked: boolean;
+  likeId: string | undefined;
 };
 
 export function useRoom(roomId: string) {
@@ -52,7 +52,7 @@ export function useRoom(roomId: string) {
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
             likeCount: Object.values(value.likes ?? {}).length,
-            hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+            likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
           };
         }
       );
