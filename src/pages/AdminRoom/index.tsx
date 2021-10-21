3bfa91cdb93b338538fed82fa3f505d35b6d79ad
André Muniz <andre250899@gmail.com>
2021-10-21 00:20:58 -0300

    add colored icons when hovering in the admin room

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -11,11 +11,11 @@ import { database } from "../../services/firebase";
 // import { useAuth } from '../hooks/useAuth';
 
 import logoImg from "../../assets/images/logo.svg";
-import deleteImg from "../../assets/images/delete.svg";
 import trashImg from "../../assets/images/delete-modal.svg";
 import dangerImg from "../../assets/images/danger.svg";
+/* import deleteImg from "../../assets/images/delete.svg";
 import answerImg from "../../assets/images/answer.svg";
-import checkImg from "../../assets/images/check.svg";
+import checkImg from "../../assets/images/check.svg"; */
 
 type RoomParams = {
   id: string;
@@ -29,7 +29,7 @@ export function AdminRoom() {
 
   const { title, questions } = useRoom(roomId);
   const [isQuestionModalVisible, setIsQuestionModalVisible] = useState(false);
-  const [isRoomModalVisible, setIsRoomModalVisible] = useState(false)
+  const [isRoomModalVisible, setIsRoomModalVisible] = useState(false);
   const [questionId, setQuestionId] = useState("");
 
   const questionDeleteModal = {
@@ -58,33 +58,32 @@ export function AdminRoom() {
     },
 
     async handleConfirmTerminateRoom() {
-        database.ref(`rooms/${roomId}`).update({
-          endedAt: new Date(),
-        });
+      database.ref(`rooms/${roomId}`).update({
+        endedAt: new Date(),
+      });
 
-        setIsRoomModalVisible(false)
-        return history.push("/");
+      setIsRoomModalVisible(false);
+      return history.push("/");
     },
-  }
+  };
 
-  function handleCheckQuestionIsAnswered(questionId : string) { 
+  function handleCheckQuestionIsAnswered(questionId: string) {
     database.ref(`rooms/${roomId}/questions/${questionId}`).update({
       isAnswered: true,
-    })
+    });
   }
 
-  function handleCheckQuestionIsHighlighted(questionId : string) {
+  function handleCheckQuestionIsHighlighted(questionId: string) {
     database.ref(`rooms/${roomId}/questions/${questionId}`).update({
       isHighlighted: true,
-    })
+    });
   }
-
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
-          <Link to='/'>
+          <Link to="/">
             <img src={logoImg} alt="Letmeask logo" />
           </Link>
           <div>
@@ -109,7 +108,12 @@ export function AdminRoom() {
         <div className="question-list">
           {questions.map((question) => {
             return (
-              <div key={question.id} className={question.isHighlighted ? 'highlighted' : 'not-highlighted'}>
+              <div
+                key={question.id}
+                className={
+                  question.isHighlighted ? "highlighted" : "not-highlighted"
+                }
+              >
                 <Question
                   key={question.id}
                   content={question.content}
@@ -123,15 +127,58 @@ export function AdminRoom() {
                     aria-label="Marcar como respondida"
                     onClick={() => handleCheckQuestionIsAnswered(question.id)}
                   >
-                    <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    <svg
+                      id="check-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="12.0003"
+                        cy="11.9998"
+                        r="9.00375"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M8.44287 12.3391L10.6108 14.507L10.5968 14.493L15.4878 9.60193"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
                   </button>
                   <button
                     className="highlited-button"
                     type="button"
                     aria-label="Marcar como lida"
-                    onClick={() => handleCheckQuestionIsHighlighted(question.id)}
+                    onClick={() =>
+                      handleCheckQuestionIsHighlighted(question.id)
+                    }
                   >
-                    <img src={answerImg} alt="Marcar pergunta como lida" />
+                    <svg
+                      id="highlight-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
                   </button>
                   <button
                     className="delete-button"
@@ -139,7 +186,29 @@ export function AdminRoom() {
                     aria-label="Deletar pergunta"
                     onClick={() => questionDeleteModal.openModal(question.id)}
                   >
-                    <img src={deleteImg} alt="" />
+                    <svg
+                      id="delete-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3 5.99988H5H21"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M8 5.99988V3.99988C8 3.46944 8.21071 2.96074 8.58579 2.58566C8.96086 2.21059 9.46957 1.99988 10 1.99988H14C14.5304 1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 2.96074 16 3.46944 16 3.99988V5.99988M19 5.99988V19.9999C19 20.5303 18.7893 21.039 18.4142 21.4141C18.0391 21.7892 17.5304 21.9999 17 21.9999H7C6.46957 21.9999 5.96086 21.7892 5.58579 21.4141C5.21071 21.039 5 20.5303 5 19.9999V5.99988H19Z"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
                   </button>
                 </Question>
               </div>
